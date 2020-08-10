f3fe22b1486440607a47b9386876e991354e53d2
xieyuecong <xieyuecong@0easy.com>
2020-08-10 10:12:53 +0800

    Initial Commit

---
+++ AsyncDrawDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AsyncDrawDemo
+//
+//  Created by SZOeasy on 2020/8/10.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AsyncDrawDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  AsyncDrawDemo
+//
+//  Created by SZOeasy on 2020/8/10.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
